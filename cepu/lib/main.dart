282d05f00423fbdfbd8d4fffa77c8a260096477a
hafis <hafiizmuhammad46@gmail.com>
2026-04-02 13:03:11 +0700

    update 1

--- cepu/lib/main.dart
+++ cepu/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Cepu App',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.green),
--- cepu/lib/screens/home_screen.dart
+++ cepu/lib/screens/home_screen.dart
@@ -1,10 +1,61 @@
+import 'package:cepu/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> signOut(BuildContext context)async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(context,
+    MaterialPageRoute(builder: (context) => SignInScreen()),
+    (route) => false 
+    );
+  }
+
+  String? _idToken = "";
+  String? _uid = "";
+  String? _email = "";
+  Future<void> getFirebaseAuthUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if(user != null) {
+      _uid = user.uid;
+      _email = user.email;
+      await user.getIdToken(true).then((v) => {setState(() {
+        _idToken = v;
+      }),
+      },
+      );
+    }
+  } 
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: (){
+              signOut(context);
+            },icon: const Icon(Icons.logout),
+          ),
+        ],
+        ),
+      body: Center(
+        child : Column(
+          children: [
+            Text("You Have Been Signed In with Token Id : ${_idToken!}"),
+            Text("Current User : ${_uid!}"),
+            Text("Current Email : ${_email!}"),
+          ],
+        ),
+      ),
+    );
   }
 }
